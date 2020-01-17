41318ec96c543bc623d473a5d4c8115b48b0435c
Adnan Mulalic <stensolino@hotmail.com>
2020-01-18 00:03:43 +0100

    Add swagger to api

--- PersonalAccounting.Api/Startup.cs
+++ PersonalAccounting.Api/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using PersonalAccounting.Api.Services;
 using PersonalAccounting.Api.Services.Interfaces;
 using PersonalAccounting.Database;
@@ -28,6 +29,11 @@ namespace PersonalAccounting.Api
             services.AddScoped<IBudgetService, BudgetService>();
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Personal Accounting API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +45,16 @@ namespace PersonalAccounting.Api
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Personal Accounting API v1");
+            });
 
             app.UseRouting();
 
