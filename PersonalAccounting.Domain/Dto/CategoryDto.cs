1c3c5779b6d73fae9deb756173797327e5a9f72c
Adnan Mulalic <stensolino@hotmail.com>
2020-05-17 19:45:23 +0200

    Add migration end extend dto for CategoryType

--- PersonalAccounting.Domain/Dto/CategoryDto.cs
+++ PersonalAccounting.Domain/Dto/CategoryDto.cs
@@ -1,6 +1,5 @@
-﻿using System;
+﻿using PersonalAccounting.Domain.Enum;
 using System.Collections.Generic;
-using System.Text;
 
 namespace PersonalAccounting.Domain.Dto
 {
@@ -10,6 +9,7 @@ namespace PersonalAccounting.Domain.Dto
         public string Name { get; set; }
         public float MaxAmount { get; set; }
         public string Description { get; set; }
+        public CategoryType Type { get; set; }
         public long BudgetId { get; set; }
         public List<TransactionDto> Transactions { get; set; }
     }
